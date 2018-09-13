fc2f40fc8285325fcdfbb65f747d864423189890
Lyuboslav Kotsev <liumbiwe@gmail.com>
2018-09-13 17:25:12 +0300

    readme created

--- src/Response.php
+++ src/Response.php
@@ -9,10 +9,11 @@ class Response
     const HEADER_SEPARATOR = "\r\n";
     const HEADERS_BODY_SEPARATOR = "\r\n\r\n";
 
-    // [Informational 1xx]
+    // Informational 1xx
     const HTTP_CONTINUE = 100;
     const HTTP_SWITCHING_PROTOCOLS = 101;
-    // [Successful 2xx]
+
+    // Successful 2xx
     const HTTP_OK = 200;
     const HTTP_CREATED = 201;
     const HTTP_ACCEPTED = 202;
@@ -20,7 +21,8 @@ class Response
     const HTTP_NO_CONTENT = 204;
     const HTTP_RESET_CONTENT = 205;
     const HTTP_PARTIAL_CONTENT = 206;
-    // [Redirection 3xx]
+
+    // Redirection 3xx
     const HTTP_MULTIPLE_CHOICES = 300;
     const HTTP_MOVED_PERMANENTLY = 301;
     const HTTP_FOUND = 302;
@@ -29,7 +31,8 @@ class Response
     const HTTP_USE_PROXY = 305;
     const HTTP_UNUSED = 306;
     const HTTP_TEMPORARY_REDIRECT = 307;
-    // [Client Error 4xx]
+
+    // Client Error 4xx
     const errorCodesBeginAt = 400;
     const HTTP_BAD_REQUEST = 400;
     const HTTP_UNAUTHORIZED = 401;
@@ -49,7 +52,8 @@ class Response
     const HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
     const HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
     const HTTP_EXPECTATION_FAILED = 417;
-    // [Server Error 5xx]
+
+    // Server Error 5xx
     const HTTP_INTERNAL_SERVER_ERROR = 500;
     const HTTP_NOT_IMPLEMENTED = 501;
     const HTTP_BAD_GATEWAY = 502;
@@ -58,15 +62,15 @@ class Response
     const HTTP_VERSION_NOT_SUPPORTED = 505;
 
     private static $messages = array(
-        // [Informational 1xx]
+        // Informational 1xx
         100 => '100 Continue', 101 => '101 Switching Protocols',
-        // [Successful 2xx]
+        // Successful 2xx
         200 => '200 OK', 201 => '201 Created', 202 => '202 Accepted', 203 => '203 Non-Authoritative Information', 204 => '204 No Content', 205 => '205 Reset Content', 206 => '206 Partial Content',
-        // [Redirection 3xx]
+        // Redirection 3xx
         300 => '300 Multiple Choices', 301 => '301 Moved Permanently', 302 => '302 Found', 303 => '303 See Other', 304 => '304 Not Modified', 305 => '305 Use Proxy', 306 => '306 (Unused)', 307 => '307 Temporary Redirect',
-        // [Client Error 4xx]
+        // Client Error 4xx
         400 => '400 Bad Request', 401 => '401 Unauthorized', 402 => '402 Payment Required', 403 => '403 Forbidden', 404 => '404 Not Found', 405 => '405 Method Not Allowed', 406 => '406 Not Acceptable', 407 => '407 Proxy Authentication Required', 408 => '408 Request Timeout', 409 => '409 Conflict', 410 => '410 Gone', 411 => '411 Length Required', 412 => '412 Precondition Failed', 413 => '413 Request Entity Too Large', 414 => '414 Request-URI Too Long', 415 => '415 Unsupported Media Type', 416 => '416 Requested Range Not Satisfiable', 417 => '417 Expectation Failed',
-        // [Server Error 5xx]
+        // Server Error 5xx
         500 => '500 Internal Server Error', 501 => '501 Not Implemented', 502 => '502 Bad Gateway', 503 => '503 Service Unavailable', 504 => '504 Gateway Timeout', 505 => '505 HTTP Version Not Supported'
     );
 
